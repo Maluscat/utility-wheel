a2b7b95e04eae75a3487e71c543cc8dc0db65e98
Hallo89 <hallo89@protonmail.com>
2023-03-15 01:30:33 +0100

    Rewrite `UtilityWheel` to TypeScript

--- script/UtilityWheel.ts
+++ script/UtilityWheel.ts
@@ -1,52 +1,62 @@
 'use strict';
-class UtilityWheel {
-  #sectionsTarget;
-  #sectionsContent;
+interface ConfigOptions {
+  target: EventTarget;
+  invokeButton: number;
+};
 
-  callbacks = {};
+type Section<T> = {
+  [key in SectionSide]: T;
+};
+
+type SectionSide = 'top' | 'right' | 'bottom' | 'left';
+
+class UtilityWheel {
+  #sectionsTarget: Section<HTMLElement>;
+  #sectionsContent: Section<HTMLElement>;
+  callbacks: Partial<Section<Function>> = {};
 
   invokeButton;
 
   element;
 
-  constructor(element, { target = window, invokeButton = 2 } = {}) {
+  constructor(element: HTMLElement, { target = window, invokeButton = 2 }: Partial<ConfigOptions> = {}) {
     this.element = element;
     this.invokeButton = invokeButton;
 
     this.#sectionsTarget = {
-      top: element.querySelector('.section-target.top'),
-      right: element.querySelector('.section-target.right'),
-      bottom: element.querySelector('.section-target.bottom'),
-      left: element.querySelector('.section-target.left'),
+      top: element.querySelector('.section-target.top')!,
+      right: element.querySelector('.section-target.right')!,
+      bottom: element.querySelector('.section-target.bottom')!,
+      left: element.querySelector('.section-target.left')!,
     };
-
     this.#sectionsContent = {
-      top: element.querySelector('.section-content.top'),
-      right: element.querySelector('.section-content.right'),
-      bottom: element.querySelector('.section-content.bottom'),
-      left: element.querySelector('.section-content.left'),
+      top: element.querySelector('.section-content.top')!,
+      right: element.querySelector('.section-content.right')!,
+      bottom: element.querySelector('.section-content.bottom')!,
+      left: element.querySelector('.section-content.left')!,
     };
 
     this.pointerDown = this.pointerDown.bind(this);
     this.invoke = this.invoke.bind(this);
     this.hide = this.hide.bind(this);
 
+    // @ts-ignore
     target.addEventListener('pointerdown', this.pointerDown);
     target.addEventListener('pointerup', this.hide);
 
     for (const [ side, section ] of Object.entries(this.#sectionsTarget)) {
-      section.addEventListener('pointerup', this.sectionUp.bind(this, side));
+      section.addEventListener('pointerup', this.sectionUp.bind(this, <SectionSide> side));
     }
   }
 
   // ---- Methods ----
-  setSection(side, node, callback) {
+  setSection(side: SectionSide, node: HTMLElement, callback: Function) {
     this.#sectionsContent[side].replaceChildren(node);
     this.callbacks[side] = callback;
   }
 
   // ---- Visibility handling ----
-  invoke(x, y) {
+  invoke(x: number, y: number) {
     this.element.classList.remove('hidden');
     this.element.style.transform = `translate(${x}px, ${y}px)`;
   }
@@ -55,13 +65,13 @@ class UtilityWheel {
   }
 
   // ---- Events ----
-  pointerDown(e) {
+  pointerDown(e: PointerEvent) {
     if (e.button === this.invokeButton) {
       this.invoke(e.clientX, e.clientY);
     }
   }
 
-  sectionUp(side) {
+  sectionUp(side: SectionSide) {
     this.callbacks[side]?.();
   }
 }
